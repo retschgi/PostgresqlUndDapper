933a7197df353490b6166ca3f9cafe957ff7fe86
Reto Gurtner <privat@retschgi.com>
2016-01-06 14:47:18 +0100

    Sicherstellen, dass Employee-Model eine BaseEntity ist

--- src/PostgresqlUndDapper/Models/Employee.cs
+++ src/PostgresqlUndDapper/Models/Employee.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using PostgresqlUndDapper.Repositories;
 
 namespace PostgresqlUndDapper.Models
 {
-    public class Employee
+    public class Employee : BaseEntity<int>
     {
-        public int Id { get; set; }
-
         public string Name { get; set; }
 
         public int YearOfContract { get; set; }
