6b2c808e467a721fe807af8bd8bc2b9c0ac876b1
Reto Gurtner <privat@retschgi.com>
2016-01-06 14:46:12 +0100

    Implementation des EmployeeRepository

---
+++ src/PostgresqlUndDapper/RepositoriesPostgresql/EmployeeRepository.cs
@@ -0,0 +1,21 @@
+﻿using PostgresqlUndDapper.Repositories;
+using PostgresqlUndDapper.Models;
+
+namespace PostgresqlUndDapper.RepositoriesPostgresql
+{
+    public class EmployeeRepository : AbstractPostgresqlRepository<Employee, int>, IEmployeeRepository
+    {
+        public EmployeeRepository(string connectionString) : base(connectionString)
+        {
+
+        }
+
+        protected override string TableName
+        {
+            get
+            {
+                return "Employees";
+            }
+        }
+    }
+}
