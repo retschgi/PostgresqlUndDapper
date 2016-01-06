7c9a9d6739276a8df0e21f8080496ed58c86768f
Reto Gurtner <privat@retschgi.com>
2016-01-06 15:03:35 +0100

    Employee-Controller mit zugehöriger View

---
+++ src/PostgresqlUndDapper/Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using PostgresqlUndDapper.Repositories;
+using Microsoft.AspNet.Mvc;
+using PostgresqlUndDapper.Models;
+
+
+namespace PostgresqlUndDapper.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private IEmployeeRepository _employeeRepository;
+
+        public EmployeeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            IEnumerable<Employee> result = _employeeRepository.Get();
+            return View(result);
+        }
+    }
+}
